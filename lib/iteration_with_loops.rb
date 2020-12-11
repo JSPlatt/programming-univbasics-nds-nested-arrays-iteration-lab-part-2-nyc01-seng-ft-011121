db225e8752c1051fcc214a4946a84dff82a6b6f9
Jeffrey Platt <jsplatt@gmail.com>
2020-12-11 15:54:15 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,9 @@
-def find_min_in_nested_arrays(src)
+ def find_min_in_nested_arrays(src)
+array_of_daily_temperatures = []
+
+  
+  
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
-
+  
 end
